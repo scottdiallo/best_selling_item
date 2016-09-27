fb5113062e327ee12df7ca3158c5badf7fe6e7da
Scott Diallo <diallot@me.com>
2016-09-27 10:25:58 -0700

    item label styled to desire taste

--- public/app.js
+++ public/app.js
@@ -31,20 +31,15 @@ $(document).ready(function () {
                     console.log(item);
                     //call the following function (showQuestion(item)) to show the API results
                     resultElement += '<li>';
-                    resultElement += '<p>' + item.names.title + '</p>';
-                    resultElement += '<p>' + item.prices.current + '</p>';
-                    resultElement += '<p>' + item.prices.regular + '</p>';
-                    resultElement += '<p>' + item.offers[0].condition + '</p>';
+                    resultElement += '<p class="title">' + item.names.title + '</p>';
+                    resultElement += "<strong>" + '<p> Current Price: $' + "</strong>" + item.prices.current + '</p>';
+                    resultElement += "<strong>" + '<p> Regular Price: $' + "</strong>" + item.prices.regular + '</p>';
+                    resultElement += "<strong>" + '<p> Condition: ' + "</strong>" + item.offers[0].condition + '</p>';
 
-                    resultElement += '<p>' + item.customerReviews.averageScore + '</p>';
-                    resultElement += '<p>' + item.descriptions.short + '</p>';
+                    resultElement += "<strong>" + '<p> Customer Reviews: ' + "</strong>" + item.customerReviews.averageScore + '</p>';
+                    resultElement += "<strong>" + '<p> Descriptions: ' + "</strong>" + item.descriptions.short + '</p>';
 
                     resultElement += '<a href="' + item.links.web + '"><img src="' + item.images.standard + '"></a>';
-
-                    //resultElement += '<p>' + item.links.web + '</p>';
-                    //                    resultElement += '<img src="' + item.images.standard + '">';
-
-                    //<img src='" + video.snippet.thumbnails.default.url + "'></a>
                     resultElement += '</li>';
 
                     $('.displayResults').append(resultElement);
